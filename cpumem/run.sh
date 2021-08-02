dac0652ea64cb8b4c59cef6b5fccde43e117e8a0
serkilov@yandex.ru <serkilov@yandex.ru>
2021-08-02 15:10:46 +0000

    Removed comments

--- cpumem/run.sh
+++ cpumem/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-img=beekman9527/cpumemload:latest
-env="-e RUN_TYPE=cpu -e CPU_PERCENT=50"
-docker run -d --cpus="0.1" $env $img 
+img=serkilov/cpumemload:latest
+env="-e MEMORY_NUM=50 -e CPU_PERCENT=1"
+docker run $env $img 
--- cpumem/src/run.sh
+++ cpumem/src/run.sh
@@ -3,8 +3,6 @@
 logfile="run.log"
 
 echo "[`date`] begin to run test"
-echo "Mem: "$MEMORY_NUM
-echo "CPU: "$CPU_PERCENT
 if [ "X$MEMORY_NUM" = "X" ] ; then
     MEMORY_NUM=100
 fi
@@ -18,8 +16,6 @@ cpumembin=cpumembin
 
 gcc cpumem.c -o $cpumembin
 chmod +x $cpumembin
-echo "Mem: "$MEMORY_NUM
-echo "CPU: "$CPU_PERCENT
 ./$cpumembin $CPU_PERCENT $MEMORY_NUM | tee $logfile
 
 echo "[`date`] end of running"
